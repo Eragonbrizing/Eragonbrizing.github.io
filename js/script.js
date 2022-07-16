debdf938393771b70606eb3ce505e2e7852292c0
Eragonbrizing <109418730+Eragonbrizing@users.noreply.github.com>
2022-07-16 15:21:24 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('.burger__button').click(function(event) {
+        $('.menu__list, .burger__button').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
+
+function ibg(){
+
+    $.each($('.ibg'), function(index, val) {
+        if($(this).find('img').length>0){
+            $(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
+        }
+    });
+}
+    
+ibg();
